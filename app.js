cfe428045e11bcfcfefe474e5e02891653fb06ea
Daniel Robles <daaan064@gmail.com>
2021-04-12 11:14:36 -0400

    second commit

--- app.js
+++ app.js
@@ -1,1 +1,52 @@
-console.log('Holiiiiiiiiiiiiii');
+// Type Assigment and Type Inference
+// let object1: object = {
+//     name: "Daniel",
+//     age: 23
+// }
+var object2 = {
+    name: "Daniel",
+    age: 23
+};
+console.log(object2.name);
+// Array 
+var firstArray = ["Daniel", " Robles"];
+var secondArray = [3, 2];
+console.log(firstArray[0]);
+// Tuplas
+var role = [1, "Designer"];
+// Enum = Asigns a value (by default it is a number and it starts with 0) for the names or key
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+;
+var Role2;
+(function (Role2) {
+    Role2[Role2["ADMIN"] = 5] = "ADMIN";
+    Role2[Role2["READ_ONLY"] = 6] = "READ_ONLY";
+    Role2[Role2["AUTHOR"] = 7] = "AUTHOR";
+})(Role2 || (Role2 = {}));
+;
+var Role3;
+(function (Role3) {
+    Role3["ADMIN"] = "ADMIN";
+    Role3[Role3["READ_ONLY"] = 5] = "READ_ONLY";
+    Role3[Role3["AUTHOR"] = 10] = "AUTHOR";
+})(Role3 || (Role3 = {}));
+;
+// Any
+var randomArray = ['Hola', 2, 'Daniel'];
+// Union 
+function combine(input1, input2) {
+    var result;
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+}
+console.log(combine('hola', 2));
--- app.ts
+++ app.ts
@@ -1,1 +1,49 @@
-console.log('Holiiiiiiiiiiiiii');
+// Type Assigment and Type Inference
+
+        // let object1: object = {
+        //     name: "Daniel",
+        //     age: 23
+        // }
+
+let object2: {name: string; age: number;} = {
+    name: "Daniel",
+    age: 23
+}
+
+console.log(object2.name);
+
+
+// Array 
+
+let firstArray: string[] = ["Daniel", " Robles"];
+let secondArray = [3, 2];
+console.log(firstArray[0]);
+
+// Tuplas
+
+let role: [number, string] = [1, "Designer"];
+
+// Enum = Asigns a value (by default it is a number and it starts with 0) for the names or key
+
+enum Role {ADMIN, READ_ONLY, AUTHOR};
+enum Role2 {ADMIN = 5, READ_ONLY, AUTHOR};
+enum Role3 {ADMIN = 'ADMIN', READ_ONLY= 5, AUTHOR= 10};
+
+// Any
+
+let randomArray: any[] = ['Hola', 2, 'Daniel'];
+
+// Union 
+
+function combine(input1: number | string, input2: number | string){
+    let result;
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+        result = input1 + input2;
+    }else{
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+}
+
+
+console.log(combine('hola', 2));
