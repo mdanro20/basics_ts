bee429321b5cb94cf1fee9f69caa4a7c7e5ed797
Daniel Robles <daaan064@gmail.com>
2021-04-14 22:27:31 -0400

    commit 14_04

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 "use strict";
 console.log('Init page');
+// If you want to execute:
+// -- just one page then put the command: tsc page.ts
+// -- just one page but changing changes automatically: tsc page.ts -w
+// -- execute this command to create a json that permit you execute all the project and not only a page: tsc --init 
+// -- execute this command to compile all ts files: tsc -w
--- app.ts
+++ app.ts
@@ -1,1 +1,6 @@
 console.log('Init page');
+// If you want to execute:
+// -- just one page then put the command: tsc page.ts
+// -- just one page but changing changes automatically: tsc page.ts -w
+// -- execute this command to create a json that permit you execute all the project and not only a page: tsc --init 
+// -- execute this command to compile all ts files: tsc -w
